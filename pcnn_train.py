a94e2c2af30402bfacbee6100f5882746f2d53fb
im-calvin <kelvinwong0519@gmail.com>
2025-04-02 20:08:52 -0700

    cast to string

--- pcnn_train.py
+++ pcnn_train.py
@@ -28,7 +28,7 @@ def train_or_test(
     for batch_idx, (model_input, labels) in enumerate(tqdm(data_loader)):
         model_input = model_input.to(device)
         # Convert string labels to numeric indices and create a tensor
-        numeric_labels = torch.tensor([my_bidict[label] for label in labels], dtype=torch.long).to(device)
+        numeric_labels = torch.tensor([my_bidict[str(label)] for label in labels], dtype=torch.long).to(device)
         model_output = model(model_input, labels=numeric_labels)
         loss = loss_op(model_input, model_output)
         loss_tracker.update(loss.item() / deno)
